f09bd823943fda04472798bc137df1ba3760d1d8
rwhitlockcompass <reesewhitlock@compassinstitution.com>
2023-05-18 05:44:18 -0600

    navbar

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -1,4 +1,5 @@
-import logo from "../../public/compass-logo.svg";
+import { loadGetInitialProps } from "next/dist/shared/lib/utils";
+import Image from "next/Image";
 
 function refreshPage() {
   window.scrollTo(0, 0);
@@ -10,36 +11,53 @@ const applicationLink =
 
 export default function Navbar() {
   return (
-    <nav className="sticky top-0 w-full border-b-1 z-50 pb-5 pt-3 bg-white shadow-sm ">
-      <div className="container flex flex-col md:flex-row md:justify-between">
-        <div className="flex items-center justify-center md:justify-start font-snas">
-          <div
-            onClick={refreshPage}
-            className="basis-1/3 flex items-center"
-          ></div>
-        </div>
-        <div className="flex items-center justify-between md:justify-end">
-          <button onClick={refreshPage} className="px-4 hover:underline">
-            ABOUT
-          </button>
-          <button className="px-4 hover:underline">EVENTS</button>
-          <button className="px-4 hover:underline">PROJECTS</button>
-          <button
-            onClick={() => window.open(applicationLink, "_blank", "noreferrer")}
-            className="px-4 hover:underline"
-            style={{ whiteSpace: "nowrap" }}
-          >
-            JOIN US
-          </button>
-          {/* <button className="px-4 flex items-center rounded ">
+    <div className="bg-gradient-to-r  from-indigo-300 via-purple-700 to-indigo-900">
+      <nav className="sticky top-0 w-full border-b-1 z-50 pb-5 pt-3 shadow-sm">
+        <div className="container flex flex-col md:flex-row md:justify-between ">
+          <div className="flex items-center justify-center md:justify-start font-snas">
+            <div
+              onClick={refreshPage}
+              className="basis-1/3 flex items-center"
+            ></div>
+          </div>
+          <div className="flex items-center justify-center md:justify-start ">
+            <Image src="/navbarLogo.png" width="150" height="15" />
+          </div>
+          <div className="flex items-center justify-between md:justify-end ">
+            <button
+              onClick={refreshPage}
+              className="px-4 hover:underline  border-yellow-300 border-2 text-yellow-300 rounded-xl"
+            >
+              ABOUT
+            </button>
+            <div className="w-10"></div>
+            <button className="px-4 hover:underline border-yellow-300 border-2 text-yellow-300 rounded-xl">
+              EVENTS
+            </button>
+            <div className="w-10"></div>
+            <button className="px-4 hover:underline border-yellow-300 border-2 text-yellow-300 rounded-xl">
+              PROJECTS
+            </button>
+            <div className="w-10"></div>
+            <button
+              onClick={() =>
+                window.open(applicationLink, "_blank", "noreferrer")
+              }
+              className="px-4 hover:underline rounded-xl bg-yellow-300"
+              style={{ whiteSpace: "nowrap" }}
+            >
+              JOIN US
+            </button>
+            {/* <button className="px-4 flex items-center rounded ">
                         <div className="w-9">
                         </div>
                         <div className="rounded ring-offset-2 ring-2 ring-sky-900 ring-opacity-70 w-28 bg-gradient-to-tr from-gray-100 to-sky-100 hover:underline">
                            SUPPORT US
                         </div>
                     </button> */}
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import "../styles/Home.module.css";
 import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+//import Footer from "./components/Footer";
 
 export default function Home() {
   return (
@@ -10,8 +10,7 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Navbar/>
-      <Footer/>
+      <Navbar />
     </div>
   );
 }
